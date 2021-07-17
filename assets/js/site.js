193b66b1ee9805587e21e466a571670582efd4ef
AjayNP <ajay.mca02@gmail.com>
2021-07-17 13:02:18 +0530

    first commit

---
+++ assets/js/site.js
@@ -0,0 +1,16 @@
+$(document).on('click', '.expand-action', function(e){
+    $(this).next('.card-header').find('.btn').trigger('click');
+
+    $('.expand-action').addClass('closed fa-plus').removeClass('opened fa-minus');
+
+    if($(this).hasClass('fa-minus')){
+        $(this).addClass('closed fa-plus').removeClass('opened fa-minus');
+    }
+    else if($(this).hasClass('fa-plus')){
+        $(this).addClass('opened fa-minus').removeClass('closed fa-plus');
+    }
+})
+
+$(document).on('click', '.expand-action.fa.fa-minus', function(e){
+    $(this).removeClass('fa-minus').addClass('fa-plus');
+})
